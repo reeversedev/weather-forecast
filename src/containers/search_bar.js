4d867ea7336225a36641b35e920107237e3fdce8
root <gogia.prateek@hotmail.com>
2018-06-16 11:25:12 +0530

    Added Search bar as a container

---
+++ src/containers/search_bar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: "" // short form of 'search term'
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+        term: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          placeholder="Get a five-day forcast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="button" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
